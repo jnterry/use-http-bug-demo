b953be9263905bf7f4df70c6b373e675faa11378
Jamie Terry <jt@terry.cloud>
2020-04-22 22:14:02 +0100

    Add minimial example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { useFetch } from 'use-http';
+
+function RouteA(){
+  return (
+    <div>
+      Hello from route A
+    </div>
+  );
+}
+
+function RouteB(){
+
+  const fetch = useFetch('https://dns.google.com/resolve?name=www.google.com', { data: null }, []);
+
+  let content = null;
+  if(fetch.data == null || fetch.loading){
+    console.log("Hit loading case");
+    console.dir(fetch.data);
+    console.dir(fetch.loading);
+    return "Loading...";
+  } else {
+    return JSON.stringify(fetch.data, null, 4);
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+
+      <Link to='/a'>Go to A</Link>
+      <br/>
+      <Link to='/b'>Go to B</Link>
+      <br/>
+      <hr/>
+
+      <Route path='/a'><RouteA/></Route>
+      <Route path='/b'><RouteB/></Route>
+		</BrowserRouter>
+	);
 }
 
 export default App;
